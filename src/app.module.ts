299e03597cfe02cf654c400e8f4fc2e1aba55ab4
Kdu Santos <anjoazrael10@gmail.com>
2025-12-30 23:52:33 -0300

    feat(devtools): configurado devtools

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { LeadsModule } from './leads/leads.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    LeadsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
